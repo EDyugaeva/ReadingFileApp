1f3f4c522f8847c3402eed82e70489bd4549b8dc
EDyugaeva <ed2408@yandex.ru>
2023-02-21 22:27:37 +0500

    some refactoring

--- src/main/java/org/example/ListCreator.java
+++ src/main/java/org/example/ListCreator.java
@@ -1,13 +1,51 @@
 package org.example;
 
+import java.io.*;
 import java.util.*;
 
 import static java.util.Comparator.comparing;
 
 public class ListCreator {
 
+    public void divideFileIntoGroups(String path) {
+        HashSet<String> hashSet = readFile(path);//читаем файл
+        List<List<String>> listOfLists = findAndGroupLines(new ArrayList<>(hashSet));//делим его на группы
+        createFile(listOfLists);
 
-    public List<List<String>> findAndGroupLines(List<String> lines) {
+        printResults(listOfLists);
+
+
+    }
+
+    private void printResults(List<List<String>> listOfLists) {
+        int size = listOfLists.size();
+        System.out.println("Количество групп = " + size);
+        for (int i = 1; i < size + 1; i++) {
+            System.out.println("Группа " + i);
+            for (String lines :
+                    listOfLists.get(i - 1)) {
+                System.out.println(lines);
+            }
+            System.out.println();
+        }
+    }
+
+    private HashSet<String> readFile(String path) {
+        HashSet<String> set = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                set.add(line);
+                line = br.readLine();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return set;
+    }
+
+
+    private List<List<String>> findAndGroupLines(List<String> lines) {
         if (lines == null)
             return Collections.emptyList();
 
@@ -24,6 +62,7 @@ public class ListCreator {
             String[] lineElements = line.split(";");
             TreeSet<Integer> groupsWithSameElems = new TreeSet<>(); //список групп, имеющих совпадающие элементы
             List<NewElements> newElements = new ArrayList<>(); //список элементов, которых нет в мапах столбцов
+            boolean correctLine = true;
 
             for (int elmentIndex = 0; elmentIndex < lineElements.length; elmentIndex++) {
                 String currentLineElement = lineElements[elmentIndex];
@@ -31,8 +70,13 @@ public class ListCreator {
                 if (columns.size() == elmentIndex)
                     columns.add(new HashMap<>());
 
+
                 if ("\"\"".equals(currentLineElement)) {
                     continue;
+                }
+
+                if ((currentLineElement.length() > 2 && currentLineElement.substring(1, currentLineElement.length() - 1).contains("\""))) {
+                    correctLine = false;
                 }
 
                 Map<String, Integer> currCol = columns.get(elmentIndex); //получаем мап с соответствием уникальных элементов и номеров групп
@@ -45,32 +89,61 @@ public class ListCreator {
                     newElements.add(new NewElements(currentLineElement, elmentIndex));//Если такой группы нет, создаем новый элемент
                 }
             }
-            int groupNumber;
-            if (groupsWithSameElems.isEmpty()) {
-                linesGroups.add(new ArrayList<>());
-                groupNumber = linesGroups.size() - 1;
-            } else {
-                groupNumber = groupsWithSameElems.first();
-            }
-            for (NewElements newLineElement : newElements) { //добавляет данные о несгруппированных элементах
-                columns.get(newLineElement.getPosition()).put(newLineElement.getString(), groupNumber);
-            }
-            for (int matchedGrNum : groupsWithSameElems) { //перебираем все группы с таким же элементом
-                     if (matchedGrNum != groupNumber) {
-                    unitedGroups.put(matchedGrNum, groupNumber); //сохраняем инф-цию об объединённых группах
-                    linesGroups.get(groupNumber).addAll(linesGroups.get(matchedGrNum)); //объединяем группы
-                    linesGroups.set(matchedGrNum, null); //помечаем группу с текущим номер, как несуществующую
-                }
-            }
-            linesGroups.get(groupNumber).add(line);
-        }
-        linesGroups.removeAll(Collections.singleton(null));
-        linesGroups.removeAll(Collections.emptyList());
-//        linesGroups.removeIf(list -> list.size() == 1);
-        linesGroups.sort(comparing(List::size));
-        Collections.reverse(linesGroups);
-        return linesGroups;
+            if (correctLine) {
 
+                int groupNumber;
+                if (groupsWithSameElems.isEmpty()) {
+                    linesGroups.add(new ArrayList<>());
+                    groupNumber = linesGroups.size() - 1;
+                } else {
+                    groupNumber = groupsWithSameElems.first();
+                }
+                for (NewElements newLineElement : newElements) { //добавляет данные о несгруппированных элементах
+                    columns.get(newLineElement.getPosition()).put(newLineElement.getString(), groupNumber);
+                }
+                for (int matchedGrNum : groupsWithSameElems) { //перебираем все группы с таким же элементом
+                    if (matchedGrNum != groupNumber) {
+                        unitedGroups.put(matchedGrNum, groupNumber); //сохраняем инф-цию об объединённых группах
+                        linesGroups.get(groupNumber).addAll(linesGroups.get(matchedGrNum)); //объединяем группы
+                        linesGroups.set(matchedGrNum, null); //помечаем группу с текущим номер, как несуществующую
+                    }
+                }
+                linesGroups.get(groupNumber).add(line);
+            }
+        }
+
+        return sortList(linesGroups);
+
+    }
+
+    private static List<List<String>> sortList(List<List<String>> list) {
+        list.removeAll(Collections.singleton(null)); //Удаляем пустые группы
+            list.removeIf(l -> l.size() == 1);
+            list.sort(comparing(List::size));
+            Collections.reverse(list);
+
+        return list;
+    }
+
+    private static void createFile(List<List<String>> collection) {
+        try (
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("result.txt"))) {
+            for (int i = 1; i < collection.size() + 1; i++) {
+                bufferedWriter.append("Группа " + i);
+                bufferedWriter.newLine();
+                for (String line :
+                        collection.get(i - 1)) {
+                    bufferedWriter.append(line).append(";");
+                    bufferedWriter.newLine();
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.newLine();
+            }
+
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
     }
 
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,61 +1,17 @@
 package org.example;
 
-import java.io.*;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         long time = System.currentTimeMillis();
-        HashSet<String> hashSet = new HashSet<>();
+        ListCreator listCreator = new ListCreator();
+
         for (String str : args) {
             if (!str.equals("-Xmx1G")) {
-                try (
-                        BufferedReader br = new BufferedReader(new FileReader(str))) {
-                    String line = br.readLine();
-                    while (line != null) {
-                        hashSet.add(line);
-                        line = br.readLine();
-                    }
-                } catch (IOException ex) {
-                    System.out.println(ex.getMessage());
-                }
-
+                listCreator.divideFileIntoGroups(str);
             }
         }
-        List<String> lines = new ArrayList<>(hashSet);
+        System.out.println("Время выполнения программы = " + (System.currentTimeMillis() - time) + " мс");
 
-        ListCreator listCreator = new ListCreator();
-        List<List<String>> result = listCreator.findAndGroupLines(lines);
-
-        System.out.println("file size = " + lines.size());
-        System.out.println("list group size = " + result.size());
-        createFile(result);
-        System.out.println(System.currentTimeMillis() - time);
-
-    }
-
-
-    public static void createFile(List<List<String>> collection) {
-        try (
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("text.txt"))) {
-            for (int i = 1; i < collection.size() +1; i++) {
-                bufferedWriter.append("Группа " + i);
-                bufferedWriter.newLine();
-
-                for (String line :
-                        collection.get(i - 1)) {
-                    bufferedWriter.append(line + ";");
-                    bufferedWriter.newLine();
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.newLine();
-            }
-
-        } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
-        }
     }
 
 
